b11b8bbfb7140f11bc9126604f159f22213dc55a
ajh1000 <moulawola@gmail.com>
2021-06-06 21:34:57 +0900

    secured secret uri

--- backend/db/dbUtil.js
+++ backend/db/dbUtil.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
-var url = 'mongodb+srv://admin:E8Dh22i-fSbjL6x@cluster0.ylv2g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const name = 'projects/1035293153873/secrets/MONGODB_ATLAS_URI/versions/1'
 let _db;
 
-const mongooseConenct = (cb) => {
+const mongooseConenct = async (cb) => {
+    const {
+        SecretManagerServiceClient
+    } = require('@google-cloud/secret-manager');
+    // Instantiates a client
+    const client = new SecretManagerServiceClient();
+
+    const [version] = await client.accessSecretVersion({
+        name: name,
+    });
+
+    // Extract the payload as a string.
+    const payload = version.payload.data.toString();
+
     mongoose.set('useCreateIndex', true);
-    mongoose.connect(url, {
+    mongoose.connect(payload, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
@@ -14,7 +28,7 @@ const mongooseConenct = (cb) => {
             cb();
         })
         .catch((err) => {
-            console.log('Cannot connect to the database.\n' ,err);
+            console.log('Cannot connect to the database.\n', err);
             process.exit();
         });
 };
