c755c7fb1838b20c01b3b91e7e7e3a3242789922
ajh1000 <moulawola@gmail.com>
2021-06-06 19:35:07 +0900

    deleted frontend version 2

--- frontend2/src/main.js
+++
@@ -1,8 +0,0 @@
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router  from './router'
-import store from '@/store' //To install Vuex to a Vue instance, pass the store instead of Vuex.
-
-createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
